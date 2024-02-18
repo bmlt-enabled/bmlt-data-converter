1f72dcb7398ec08c565c8f37d13da7b09be65a7e
Patrick Joyce <pjaudiomv@gmail.com>
2024-02-18 09:44:56 -0500

    fix favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 	<link rel="apple-touch-icon" href="apple-icon-180.png" />
 	<meta name="mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="icon" href="favicon.png" />
 	<title>BMLT Data Converter</title>
-	<script src="https://use.fontawesome.com/releases/v6.5.1/js/all.js" data-auto-a11y="true"></script>
 </svelte:head>
 
 <DataConverter />
